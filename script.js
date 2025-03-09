c147724e650b58d3ae61f863d675d9c3f3a1799a
switchmind1973 <153269391+switchmind1973@users.noreply.github.com>
2025-03-09 09:43:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js')
+      navigator.serviceWorker.register('/ChangeOilMonitoring/sw.js')
         .then((registration) => {
           console.log('Service Worker registered:', registration);
         })
